2177e38f6c6046dab2dd62fe367199f28d4eb0b5
Samson Ayodele <idowusamson0777@gmail.com>
2022-08-11 22:56:02 +0100

    another commit

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 const db = new Dexie('ShoppingApp')
+db.version(1).stores( { items: '++id,name,price,isPurchased'} )
+
+const itemForm = document.getElementById( 'itemForm' )
+const itemsDiv = document.getElementById( 'itemsDiv' )
+const totalPriceDiv = document.getElementById( 'totalPriceDiv' )
+
+const populateItemsDiv = async () => {
+    const allItems = await db.items.reverse().toArray()
+    // [
+    //     {
+    //         name:'table tissue',
+    //         quantity: 5,
+    //         price: 70,
+    //         isPurchased: true
+    //     },
+    //     {
+    //         name: 'bread',
+    //         quantity: 7,
+    //         price: 400,
+    //         isPurchased: false 
+    //     }
+    // ]
+    itemsDiv.innerHTML = allItems.map(item => `
+        <div class="item ${item.isPurchased && 'purchased'}">
+        <label>
+            <input 
+            type="checkbox" 
+            class="checkbox" 
+            ${item.isPurchased && 'checked'} >
+        </label>  
+
+        <div class="itemInfo">
+            <p>${item.name}</p>
+            <p>$${item.price} x ${item.quantity}</p>
+        </div>
+
+        <button class="deleteButton">X</button>
+    </div>
+    `)
+
+    const arrayOfPrices = allItems.map(item => item.price * item.quantity)
+    const totalPrice = arrayOfPrices.reduce((a, b) => a + b, 0)
+
+    totalPriceDiv.innerText = 'Total price: $' + totalPrice
+}
+
+window.onload = populateItemsDiv
+
+itemForm.onsubmit = async ( event, Event ) => {
+    event.preventDefault()
+
+    const name = document.getElementById( 'nameInput').value
+    const quantity = document.getElementById( 'quantityInput').value
+    const price = document.getElementById( 'priceInput').value
+
+    await db.items.add({ name, quantity, price }) 
+
+    itemForm.reset()
+}
